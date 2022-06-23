808068f612793f17296fede12462d1543b730e82
thebigbossnetwork <xhalakoh@yahoo.com>
2022-06-23 17:40:45 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+    var firstName = 'Abdulbasit';
+    var middleName = 'Moyosore';
+    var lastname = 'Salako';
+    var height = '173cm';
+    var country = 'Nigeria';
+
+    Name = firstName + ' ' + middleName + ' ' + lastname;
+
+    document.write('Name:' + ' ' + Name + '<br/>');
+    document.write('Height:' + ' ' + height + '<br>');
+    document.write('Country:' + 
+    ' ' + country);
